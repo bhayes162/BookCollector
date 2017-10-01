108dc5579f3f3f076e5ed8bb263dfc839456a553
Bryan Hayes <66hayes@gmail.com>
2017-10-01 18:05:52 -0400

    finished Book Collector in xcode 8, need xcode 9 to use camera

--- BookCollector/BookViewController.swift
+++ BookCollector/BookViewController.swift
@@ -9,20 +9,30 @@
 import UIKit
 
 class BookViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+    
+    @IBOutlet weak var addUpdateButton: UIButton!
     @IBOutlet weak var bookImageView: UIImageView!
     @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var deleteButton: UIButton!
     
     var imagePicker = UIImagePickerController()
-    
+    var book : Book? = nil
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         imagePicker.delegate = self
         
-
+        if book != nil {
+            bookImageView.image = UIImage(data: book!.image! as Data)
+            titleTextField.text = book!.title
+            
+            addUpdateButton.setTitle("Update", for: .normal)
+            
+        } else {
+            deleteButton.isHidden = true
+        }
     }
     @IBAction func photosTapped(_ sender: Any) {
         
@@ -30,7 +40,7 @@ class BookViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         present(imagePicker, animated: true, completion: nil)
         
     }
-
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         let image = info[UIImagePickerControllerOriginalImage] as! UIImage
         
@@ -41,18 +51,33 @@ class BookViewController: UIViewController, UIImagePickerControllerDelegate, UIN
     
     
     @IBAction func cameraTapped(_ sender: Any) {
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+        
     }
     
     @IBAction func addTapped(_ sender: Any) {
+        if book != nil {
+            
+            book!.title = titleTextField.text
+            book!.image = UIImagePNGRepresentation(bookImageView.image!) as NSData!
+            
+        } else {
+            
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            
+            let book = Book(context: context)
+            book.title = titleTextField.text
+            book.image = UIImagePNGRepresentation(bookImageView.image!) as NSData!
+        }
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            navigationController!.popViewController(animated: true)
+        }
         
-        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-        
-        let book = Book(context: context)
-        book.title = titleTextField.text
-        book.image = UIImagePNGRepresentation(bookImageView.image!) as NSData!
-        
+    @IBAction func deleteTapped(_ sender: Any) {
+         let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        context.delete(book!)
         (UIApplication.shared.delegate as! AppDelegate).saveContext()
         navigationController!.popViewController(animated: true)
     }
-    
 }
